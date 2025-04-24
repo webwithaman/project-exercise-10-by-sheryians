ccacf3caa411383c7c7693e8ba24a7ebc45cc7cc
aman <webwithaman06@gmail.com>
2025-04-24 17:01:54 +0530

    refactoring cod

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+// Get Elements
 let myForm = document.getElementById("myForm");
-let nameField = document.getElementById("name-field");
+let usernameField = document.getElementById("username-field");
 let emailField = document.getElementById("email-field");
 let passwordField = document.getElementById("password-field");
 let nameWarning = document.querySelector(".name-warning");
@@ -7,23 +8,115 @@ let emailWarning = document.querySelector(".email-warning");
 let passwordWarning = document.querySelector(".password-warning");
 let inputFields = document.querySelectorAll("input:not(.submit-btn)");
 
-myForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  let nameValue = nameField.value.trim();
-  if (nameValue.length === 0) {
-    nameWarning.textContent = "📝 Please enter your Name";
-    nameWarning.style.maxHeight = nameWarning.scrollHeight + "px";
+const isValidUsername = (usernameValue) => {
+  let isValid = true,
+    warningMessage,
+    minLength = 3,
+    maxLength = 30;
+
+  usernameValue = usernameValue.trim();
+
+  if (usernameValue.length === 0) {
+    isValid = false;
+    warningMessage = "📝 Please enter Username";
   } else {
     let regex = /^[A-Za-z]+$/;
-    if (!regex.test(nameValue)) {
-      nameWarning.textContent =
+    if (!regex.test(usernameValue)) {
+      isValid = false;
+      warningMessage =
         "🆎 Only letters & spaces allowed (no digits and special symbols)";
-      nameWarning.style.maxHeight = nameWarning.scrollHeight + "px";
-    } else if (nameValue.length < 3) {
-      nameWarning.textContent = "📏 Name must be at least 3 characters";
-      nameWarning.style.maxHeight = nameWarning.scrollHeight + "px";
+    } else if (usernameValue.length < minLength) {
+      isValid = false;
+      warningMessage = "📏 Username must be at least 3 characters";
+    } else if (usernameValue.length > maxLength) {
+      isValid = false;
+      warningMessage = "📏 Username cannot be larger than 30 characters";
     }
   }
+
+  return { isValid, warningMessage };
+};
+
+const isValidEmail = (emailValue) => {
+  let isValid = true,
+    warningMessage;
+
+  emailValue = emailValue.trim();
+
+  if (emailValue.length === 0) {
+    isValid = false;
+    warningMessage = "📝 Please enter Email";
+  }
+  // else {
+  //   let regex = /[A-Za-z]/;
+  //   if (!regex.test(emailValue)) {
+  //     isValid = false;
+  //     warningMessage = "Please enter Valid Email";
+  //   }
+  // }
+
+  return { isValid, warningMessage };
+};
+
+const isValidPassword = (passwordValue) => {
+  let isValid = true,
+    warningMessage;
+
+  passwordValue = passwordValue.trim();
+
+  if (passwordValue.length === 0) {
+    isValid = false;
+    warningMessage = "📝 Please enter Password";
+  }
+  // else {
+  //   let regex = /^[A-Za-z]+$/;
+  //   if (!regex.test(usernameValue)) {
+  //     isValid = false;
+  //     warningMessage =
+  //       "🆎 Only letters & spaces allowed (no digits and special symbols)";
+  //   } else if (usernameValue.length < minLength) {
+  //     isValid = false;
+  //     warningMessage = "📏 Name must be at least 3 characters";
+  //   } else if (usernameValue.length > maxLength) {
+  //     isValid = false;
+  //     warningMessage = "📏 Name cannot be larger than 30 characters";
+  //   }
+  // }
+
+  return { isValid, warningMessage };
+};
+
+const isValidForm = () => {
+  let validityStatus = isValidUsername(usernameField.value);
+
+  if (!validityStatus.isValid) {
+    nameWarning.textContent = validityStatus.warningMessage;
+    nameWarning.style.maxHeight = nameWarning.scrollHeight + "px";
+    return false;
+  }
+
+  validityStatus = isValidEmail(emailField.value);
+  if (!validityStatus.isValid) {
+    emailWarning.textContent = validityStatus.warningMessage;
+    emailWarning.style.maxHeight = emailWarning.scrollHeight + "px";
+    return false;
+  }
+
+  validityStatus = isValidPassword(passwordField.value);
+  if (!validityStatus.isValid) {
+    passwordWarning.textContent = validityStatus.warningMessage;
+    passwordWarning.style.maxHeight = passwordWarning.scrollHeight + "px";
+    return false;
+  }
+
+  return true;
+};
+
+myForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (isValidForm()) console.log("valid");
+  else console.log("not valid");
 });
 
 inputFields.forEach((element) => {
